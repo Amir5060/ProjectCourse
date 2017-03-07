49fa248f5a80a93c1747ac577b66302826fd370e
Amir <a_5060@yahoo.com>
2017-03-07 09:31:29 +0200

    Plan should work now.

--- ProjectCourse/Models/Classes/Plan.cs
+++ ProjectCourse/Models/Classes/Plan.cs
@@ -12,14 +12,19 @@ namespace ProjectCourse.Models
         /// <summary>
         /// Description:
         ///     Returns true if there is a unfinished plan for the user, and false if all the plans are finished.
+        ///     Returns false if there is not any records.
         /// History:
         ///     Amir Naji   27/02/2017
+        ///     Amir Naji   07/03/2017
         /// </summary>
         /// <param name="userId"></param>
         /// <returns></returns>
         public bool HaveUnfinishedPlan(string userId)
         {
-            return db.Plans.FirstOrDefault(x => x.UserID == UserID && x.FinishDate != null) == null ? true : false;
+            var temp = db.Plans.FirstOrDefault(x => x.UserID == UserID && x.FinishDate != null);
+            if (temp != null)
+                return db.Plans.FirstOrDefault(x => x.UserID == UserID && x.FinishDate != null) == null ? true : false;
+            return false;
         }
     }
 }
